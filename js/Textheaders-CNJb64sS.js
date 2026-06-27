18ba2f26215742f3a4617d88cd8f0cc69e106057
johntetteh090 <johntetteh090@gmail.com>
2026-06-27 18:30:27 +0000

    Updates

---
+++ js/Textheaders-CNJb64sS.js
@@ -0,0 +1,2 @@
+import{j as t,f as e}from"./index-BopGyWMo.js";const s=({text:r})=>t.jsx(e,{sx:{fontWeight:700,fontSize:{xs:24,md:28},color:"text.primary",mb:.5},children:r});export{s as T};
+//# sourceMappingURL=Textheaders-CNJb64sS.js.map
---
+++ js/appColors-avEKBl0P.js
@@ -0,0 +1,2 @@
+const o="#1976d2",r="#dc004e";export{r as d,o as p};
+//# sourceMappingURL=appColors-avEKBl0P.js.map
