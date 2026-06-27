9f8a178cfa068483919a7cb4210bd12b771f8b28
johntetteh090 <johntetteh090@gmail.com>
2026-06-27 16:03:35 +0000

    deploy 6

--- vite.config.mjs
+++ vite.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => {
   return {
     // base: isDev ? '/' : API_URL,
     // base: API_URL,
-    base: "/kofi-demo/",
+    base: "/kofi-demo",
     server: {
       open: true,
       port: PORT,
